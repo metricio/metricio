ff8ea83e3a25ba1bc8e653ec0771f0efc1243651
Ioannis Koutras <ikoutras@epages.com>
2019-03-27 14:58:07 +0100

    Remove default dashboard items and jobs

--- src/dashboards/index.jsx
+++ src/dashboards/index.jsx
@@ -4,21 +4,9 @@ import ReactDOM from 'react-dom';
 import '../styles/default.scss';
 
 import Dashboard from '../widgets/dashboard';
-import NumberWidget from '../widgets/number/widget';
-import PingWidget from '../widgets/ping/widget';
-import BuildStatusWidget from '../widgets/build-status/widget';
-import SparklineWidget from '../widgets/sparkline/widget';
-import ProgressWidget from '../widgets/progress/widget';
 
 ReactDOM.render(
   <Dashboard>
-    <SparklineWidget name="DemoUsers" title="Users" format="0.00a" />
-    <PingWidget name="GooglePing" title="API" />
-    <NumberWidget name="ReasonPRs" title="Pull Requests" />
-    <BuildStatusWidget name="DemoMaster" title="Build - Master" size="medium" />
-    <ProgressWidget name="DemoProgress" title="Sales Target" />
-    <NumberWidget name="DemoConversion" title="Conversion" metric="%" format="0.0a" />
-    <BuildStatusWidget name="DemoDevelop" title="Build - Develop" size="medium" />
   </Dashboard>,
   document.getElementById('content'),
 );
