95069d41b6728279f0c921a4ffc8297c625f46f7
Ioannis Koutras <ikoutras@epages.com>
2019-03-27 15:14:34 +0100

    Introduce CircleCI Builds Overview widget

--- src/dashboards/index.jsx
+++ src/dashboards/index.jsx
@@ -4,9 +4,20 @@ import ReactDOM from 'react-dom';
 import '../styles/default.scss';
 
 import Dashboard from '../widgets/dashboard';
+import CircleCIBuildsOverview from '../widgets/circle-ci-builds-overview/widget'
 
 ReactDOM.render(
   <Dashboard>
+    <CircleCIBuildsOverview
+      name="epages-ui-master"
+      title="epages-ui master"
+      size="medium"
+    />
+    <CircleCIBuildsOverview
+      name="ng-merchant-ui-master"
+      title="ng-merchant-ui master"
+      size="medium"
+    />
   </Dashboard>,
   document.getElementById('content'),
 );
