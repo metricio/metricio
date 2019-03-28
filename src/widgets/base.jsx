4d8f4be5b6541206dba8649e3ff83222b2fb0606
Aleksei Sasin <a.sasin@epages.com>
2019-03-28 16:20:23 +0100

    align body in tiles

--- src/widgets/base.jsx
+++ src/widgets/base.jsx
@@ -13,7 +13,35 @@ export default class BaseWidget extends React.Component {
     this.props.socket.on(`widget:update:${this.props.name}`, data => {
       logger('info', `updating widget: ${this.props.name}`, data);
       this.setState(data);
+      doHeightUpdate();
     });
+  }
+}
+
+function doHeightUpdate() {
+  var tile_bodies = document.getElementsByClassName('tile-body');
+  var last_height = -1;
+  var last_div_el = undefined;
+  var curr_height = 0;
+
+  for (var i = 1; i < tile_bodies.length + 1; i++) {
+    if (last_height == -1) {
+      last_height = tile_bodies[i - 1].offsetHeight;
+      last_div_el = tile_bodies[i - 1];
+      continue;
+    }
+
+    if (i % 2 == 0) {
+      curr_height = tile_bodies[i - 1].offsetHeight;
+      if (curr_height === last_height) {
+        continue;
+      }
+
+      var new_height = Math.max(curr_height, last_height);
+      tile_bodies[i - 1].style.height = new_height;
+      last_div_el.style.height = new_height;
+      last_height = -1;
+    }
   }
 }
 
