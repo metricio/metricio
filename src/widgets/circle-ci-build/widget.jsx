6eb95f6146f849663d0fdddc617f72e985df3ebc
Danil Minibaev <d.minibaev@epages.com>
2019-03-28 16:15:41 +0100

    Fix: Stateful ci build

--- src/widgets/circle-ci-build/widget.jsx
+++ src/widgets/circle-ci-build/widget.jsx
@@ -4,61 +4,88 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
-export default function CircleCIBuild({
-  author,
-  authorAvatar,
-  buildStatus,
-  reponame,
-  commitMessage,
-  githubPR,
-  circleCiJob,
-  buildUrl,
-  commitHash,
-}) {
-  const classList = classNames(
-    'widget__circleCIBuild',
-    'widget__circleCIBuild_tile',
-    `widget--${buildStatus}`
-  );
+export default class CircleCIBuild extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRebuilding: false,
+    };
+  }
 
-  const titleClassList = classNames('tile-title', `tile-title--${buildStatus}`);
+  static propTypes = {
+    author: PropTypes.string.isRequired,
+    authorAvatar: PropTypes.string.isRequired,
+    buildStatus: PropTypes.string.isRequired,
+    reponame: PropTypes.string.isRequired,
+    commitMessage: PropTypes.string.isRequired,
+    commitHash: PropTypes.string.isRequired,
+    buildUrl: PropTypes.string.isRequired,
+  };
 
-  const rebuild = e => {
+  rebuild = e => {
     fetch('/rebuild', {
       method: 'POST',
       body: JSON.stringify({
-        buildUri: buildUrl.replace('https://circleci.com/gh/', ''),
+        buildUri: this.props.buildUrl.replace('https://circleci.com/gh/', ''),
       }), // data can be `string` or {object}!
       headers: {
         'Content-Type': 'application/json',
       },
     })
-      .then(response => {})
+      .then(response => {
+        this.setState({ isRebuilding: true });
+      })
       .catch(error => console.error('Error:', error));
   };
 
-  return (
-    <div className={classList}>
-      <div className={titleClassList}>
-        <img
-          className="tile-title-avatar"
-          src={authorAvatar}
-          alt=""
-          height="64"
-        />{' '}
-        <p className="tile-title-name">{author}</p>
-        <div className="tile-title-control">
-          {!['success', 'fixed'].includes(buildStatus) && (
-            <button
-              disabled={['running', 'scheduled'].includes(buildStatus)}
-              onClick={rebuild}
-              className="tile-title-control-rebuild"
-            >
-              Rebuild
-            </button>
-          )}
+  render() {
+    const {
+      author,
+      authorAvatar,
+      buildStatus,
+      reponame,
+      commitMessage,
+      githubPR,
+      circleCiJob,
+      commitHash,
+    } = this.props;
+
+    const classList = classNames(
+      'widget__circleCIBuild',
+      'widget__circleCIBuild_tile',
+      `widget--${buildStatus}`
+    );
+
+    const titleClassList = classNames(
+      'tile-title',
+      `tile-title--${buildStatus}`
+    );
+
+    return (
+      <div className={classList}>
+        <div className={titleClassList}>
+          <img
+            className="tile-title-avatar"
+            src={authorAvatar}
+            alt=""
+            height="64"
+          />{' '}
+          <p className="tile-title-name">{author}</p>
+          <div className="tile-title-control">
+            {!['success', 'fixed'].includes(buildStatus) && (
+              <button
+                className="tile-title-control-rebuild"
+                disabled={
+                  this.state.isRebuilding ||
+                  ['running', 'scheduled'].includes(buildStatus)
+                }
+                onClick={this.rebuild}
+              >
+                Rebuild
+              </button>
+            )}
+          </div>
         </div>
-      </div>
 
       <div className="tile-body">
         <div className="tile-body-project">{reponame}</div>
@@ -80,9 +107,7 @@ export default function CircleCIBuild({
                 width="32"
                 aria-hidden="true"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
                 />
               </svg>
             </a>
@@ -111,13 +136,5 @@ export default function CircleCIBuild({
       </div>
     </div>
   );
+  }
 }
-
-CircleCIBuild.propTypes = {
-  author: PropTypes.string.isRequired,
-  authorAvatar: PropTypes.string.isRequired,
-  buildStatus: PropTypes.string.isRequired,
-  reponame: PropTypes.string.isRequired,
-  commitMessage: PropTypes.string.isRequired,
-  buildUrl: PropTypes.string.isRequired,
-};
